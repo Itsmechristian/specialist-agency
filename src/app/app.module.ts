0796bdbba185948ba6fe19f10d215ee53601bf0c
itsmechristian <hello@itsmechristian.co.uk>
2018-04-22 22:26:51 +0100

    intial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from '../components/navbar/navbar.component';
+import { HomeComponent } from '../components/home/home.component';
+import { AboutComponent } from '../components/about/about.component';
 
+
+import { routing } from './app.router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/components/navbar/navbar.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
+@Component({
+  selector: 'navbar-root',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+  animations: [
+    trigger('animatenavbar', [
+      state('hide', style({
+        display: 'none',
+        maxHeight: '0px'
+      })),
+      state('show', style({
+        display: 'block',
+        maxHeight: '300px'
+      })),
+      transition('hide => show', animate(200, style({
+        display: 'block',
+        maxHeight: '300px'
+      }))),
+      transition('show => hide', animate(200, style({
+        display: 'none',
+        maxHeight: '0px'        
+      })))
+    ])
+  ]
+})
+export class NavbarComponent {
+  constructor() { }
+
+  navbarState: String = 'hide';
+  
+  animateNavbar() {
+    (this.navbarState === 'hide') ? this.navbarState = 'show' : this.navbarState = 'hide';
+  }
+}
