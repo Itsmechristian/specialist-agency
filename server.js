1a153e44f9870759d9259f4dd481ad492d8f91a6
itsmechristian <hello@itsmechristian.co.uk>
2018-04-23 00:54:46 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+    , app = express()
+    , path = require('path')
+
+app.use(express.static(path.join(__dirname, 'dist')))
+
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'))
+})
+
+app.listen(5000, () => console.log('Connected to server'))
