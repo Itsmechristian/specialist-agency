9de8e00f403ab6f61888e62f197c788775bc1f77
itsmechristian <hello@itsmechristian.co.uk>
2018-04-27 22:31:19 +0100

    initial commit

--- src/components/contact/contact.component.ts
+++ src/components/contact/contact.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { environment } from '../../environments/environment';
+import {} from '@types/googlemaps';
+declare const google;
 
 @Component({
     templateUrl: './contact.component.html',
     styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent {
-    constructor() { }
+export class ContactComponent implements OnInit {
+    @ViewChild('googlemap') googlemap: any;
+    map: google.maps.Map;
+    marker: google.maps.Marker;
+
+    constructor(
+    ) { }
+
+    ngOnInit() {
+        let lat = 52.6368778
+        let lng =  -1.1397591999999577
+
+        this.map = new google.maps.Map(this.googlemap.nativeElement, {
+            center: new google.maps.LatLng(lat, lng),
+            zoom: 12,
+            gestureHandling: 'none',
+            zoomControl: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+
+        this.marker = new google.maps.Marker({
+            position: {
+                lat: lat,
+                lng: lng
+            },
+            map: this.map
+        })
+    }
 }
